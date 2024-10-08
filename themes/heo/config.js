a93d7aaae4c225d8d992abd11d78cb5b9a515553
kunkka <xiaosatufu@gmail.com>
2024-10-08 14:37:02 +0800

    Update config.js
    change config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '',
   HEO_HERO_TITLE_2: '',
   HEO_HERO_TITLE_3: '',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '',
   HEO_HERO_TITLE_LINK: '#',
 
   // 英雄区显示三个置顶分类
